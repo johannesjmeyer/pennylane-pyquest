438e212e6d0ae26c48986401c564c6e12a825602
root <root@JJ-XPS.localdomain>
2020-05-14 10:09:10 +0200

    Dev commit

--- pennylane_pyquest/pyquest_device.py
+++ pennylane_pyquest/pyquest_device.py
@@ -21,17 +21,6 @@ Pyquest device class
 
 An abstract base class for constructing Pyquest devices for PennyLane.
 
-This should contain all the boilerplate for supporting PennyLane
-from the Pyquest, making it easier to create new devices.
-The abstract base class below should contain all common code required
-by the Pyquest.
-
-This abstract base class will not be used by the user. Add/delete
-methods and attributes below where needed.
-
-See https://pennylane.readthedocs.io/en/latest/API/overview.html
-for an overview of how the Device class works.
-
 Classes
 -------
 
@@ -46,10 +35,23 @@ import itertools
 
 # we always import NumPy directly
 import numpy as np
-
 from pennylane import QubitDevice
-
+import pyquest_cffi as pqc
 from ._version import __version__
+
+class QuregContext:
+    def __init__(self, wires):
+        self.wires = wires
+
+    def __enter__(self):
+        self.env = pqc.utils.createQuestEnv()()
+        self.qureg = pqc.utils.createQureg()(wires, env=env)
+
+        return self
+
+    def __exit__():
+        pqc.utils.destroyQureg(self.qureg, env=self.env)
+        pqc.utils.destroyQuestEng(self.env)
 
 
 class PyquestDevice(QubitDevice):
@@ -71,146 +73,8 @@ class PyquestDevice(QubitDevice):
     short_name = 'pyquest.base'
     _operation_map = {}
 
-    def __init__(self, wires, *, shots=0, additional_option=2):
-        super().__init__(wires, shots)
-        self.additional_option = hbar
-        self.prog = None
-        self.state = None
-        self.samples = None
+    def __init__(self, wires, *, shots=1000, analytic=True):
+        super().__init__(wires, shots, analytic)
+        
 
-    def reset(self):
-        """Reset the device"""
-        pass
-
-    @property
-    def operations(self):
-        """Get the supported set of operations.
-
-        Returns:
-            set[str]: the set of PennyLane operation names the device supports
-        """
-        return set(self._operation_map.keys())
-
-    # ==========================================================
-    # The following methods below provide common calculations
-    # and patterns required for qubit PennyLane plugin devices.
-    # Feel free to keep and modify any provided methods below,
-    # or delete them as you see fit.
-
-    def expval(self, observable, wires, par):
-        if self.shots == 0:
-            # exact expectation value
-            eigvals = self.eigvals(observable, wires, par)
-            prob = np.fromiter(self.probabilities(wires=wires).values(), dtype=np.float64)
-            return (eigvals @ prob).real
-
-        # estimate the ev
-        return np.mean(self.sample(observable, wires, par))
-
-    def var(self, observable, wires, par):
-        if self.shots == 0:
-            # exact variance value
-            eigvals = self.eigvals(observable, wires, par)
-            prob = np.fromiter(self.probabilities(wires=wires).values(), dtype=np.float64)
-            return (eigvals ** 2) @ prob - (eigvals @ prob).real ** 2
-
-        # estimate the variance
-        return np.var(self.sample(observable, wires, par))
-
-    def rotate_basis(self, observable, wires, par):
-        """Rotates the specified wires such that they
-        are in the eigenbasis of the provided observable.
-
-        Args:
-            observable (str): the name of an observable
-            wires (List[int]): wires the observable is measured on
-            par (List[Any]): parameters of the observable
-        """
-        if observable == "PauliX":
-            # X = H.Z.H
-            self.apply("Hadamard", wires=wires, par=[])
-
-        elif observable == "PauliY":
-            # Y = (HS^)^.Z.(HS^) and S^=SZ
-            self.apply("PauliZ", wires=wires, par=[])
-            self.apply("S", wires=wires, par=[])
-            self.apply("Hadamard", wires=wires, par=[])
-
-        elif observable == "Hadamard":
-            # H = Ry(-pi/4)^.Z.Ry(-pi/4)
-            self.apply("RY", wires, [-np.pi / 4])
-
-        elif observable == "Hermitian":
-            # For arbitrary Hermitian matrix H, let U be the unitary matrix
-            # that diagonalises it, and w_i be the eigenvalues.
-            Hmat = par[0]
-            Hkey = tuple(Hmat.flatten().tolist())
-
-            if Hkey in self._eigs:
-                # retrieve eigenvectors
-                U = self._eigs[Hkey]["eigvec"]
-            else:
-                # store the eigenvalues corresponding to H
-                # in a dictionary, so that they do not need to
-                # be calculated later
-                w, U = np.linalg.eigh(Hmat)
-                self._eigs[Hkey] = {"eigval": w, "eigvec": U}
-
-            # Perform a change of basis before measuring by applying U^ to the circuit
-            self.apply("QubitUnitary", wires, [U.conj().T])
-
-    def eigvals(self, observable, wires, par):
-        """Determine the eigenvalues of observable(s).
-
-        Args:
-            observable (str, List[str]): the name of an observable,
-                or a list of observables representing a tensor product.
-            wires (List[int]): wires the observable(s) is measured on
-            par (List[Any]): parameters of the observable(s)
-
-        Returns:
-            array[float]: an array of size ``(len(wires),)`` containing the
-            eigenvalues of the observable
-        """
-        # observable should be Z^{\otimes n}
-        eigvals = z_eigs(len(wires))
-
-        if isinstance(observable, list):
-            # determine the eigenvalues
-            if "Hermitian" in observable:
-                # observable is of the form Z^{\otimes a}\otimes H \otimes Z^{\otimes b}
-                eigvals = np.array([1])
-
-                for k, g in itertools.groupby(zip(observable, wires, par), lambda x: x[0] == "Hermitian"):
-                    if k:
-                        p = list(g)[0][2]
-                        Hkey = tuple(p[0].flatten().tolist())
-                        eigvals = np.kron(eigvals, self._eigs[Hkey]["eigval"])
-                    else:
-                        n = len([w for sublist in list(zip(*g))[1] for w in sublist])
-                        eigvals = np.kron(eigvals, z_eigs(n))
-
-        elif observable == "Hermitian":
-            # single wire Hermitian observable
-            Hkey = tuple(par[0].flatten().tolist())
-            eigvals = self._eigs[Hkey]["eigval"]
-
-        elif observable == "Identity":
-            eigvals = np.ones(2 ** len(wires))
-
-        return eigvals
-
-
-@functools.lru_cache()
-def z_eigs(n):
-    r"""Returns the eigenvalues for :math:`Z^{\otimes n}`.
-
-    Args:
-        n (int): number of wires
-
-    Returns:
-        array[int]: eigenvalues of :math:`Z^{\otimes n}`
-    """
-    if n == 1:
-        return np.array([1, -1])
-    return np.concatenate([z_eigs(n - 1), -z_eigs(n - 1)])
+    def apply(self, operations, rotations=None, **kwargs):
